79ada917df7be7b7e39bcca6e6995ec4e47fc213
Djura Smits <djura.smits@gmail.com>
2020-05-25 17:19:33 +0200

    Added mock cbs node

--- run.sh
+++
@@ -1,8 +0,0 @@
-# Install vantage6
-pip install -r requirements.txt
-
-# Run server with included configuration
-vserver start -c server-conf.yml
-
-
-python setup_vserver.py
--- setup_vserver.py
+++ setup_vserver.py
@@ -1,15 +1,28 @@
 import requests
 import json
 
+ORGANIZATION = {'address1': 'my address 1, Amsterdam',
+                'country': 'the Netherlands',
+                'name': 'NLEsC',
+                'zipcode': '1234ab'}
+ADMIN_USER = {'firstname': 'djura',
+              'lastname': 'smits',
+              'password': 'admin',
+              'username': 'admin',
+              'roles': ['admin']
+              }
+
 DEFAULT_USERNAME = 'root'
 DEFAULT_PASSWORD = 'root'
 HOST = 'localhost'
 PORT = 5000
 PREFIX = 'api'
 ENCODING = 'utf-8'
-OK = 200
+OK_RESPONSES = [200, 201]
 POST = 'POST'
 SERVER_ROOT = f'{HOST}:{PORT}/{PREFIX}/'
+COLLABORATION_NAME = 'collab1'
+NUM_NODES = 2
 
 
 def get_url(endpoint) -> str:
@@ -22,33 +35,62 @@ def request(endpoint, payload, headers=None, method='GET') -> dict:
 
     url = get_url(endpoint)
 
-    print(f'Request url: {url}')
+    print(f'Request {method} {url}')
 
     headers['Content-Type'] = 'application/json'
     response = requests.request(method, url, headers=headers, data=json.dumps(payload))
 
-    if response.status_code == OK:
+    if response.status_code in OK_RESPONSES:
         return response.json()
     else:
         raise Exception(f'Request returned status {response.status_code}\nMessage: {response.content}')
 
 
-def main():
-    # Get user token
-    result = request('user/token', {'username': DEFAULT_USERNAME, 'password': DEFAULT_PASSWORD}, method=POST)
+def get_token(username, password):
+    result = request('token/user', {'username': username, 'password': password}, method=POST)
     access_token = result['access_token']
     bearer_token = f'Bearer {access_token}'
+    return bearer_token
+
+
+def main():
+    # Get user token
+    bearer_token = get_token(DEFAULT_USERNAME, DEFAULT_PASSWORD)
 
     # Create organization
-    organization = {'address1': 'my address 1, Amsterdam',
-                    'country': 'the Netherlands',
-                    'name': 'NLEsC',
-                    'zipcode': '1234ab'}
+    organization = ORGANIZATION
 
-    result = request('organization', organization, {'Authorization': bearer_token}, POST)
+    headers = {'Authorization': bearer_token}
+    result = request('organization', organization, headers, POST)
     organization_id = result['id']
 
-    print(result)
+    print(f'Created organization with id {organization_id}')
+
+    # Create collaboration
+    collaboration = {'name': COLLABORATION_NAME, 'organization_ids': [organization_id]}
+    result = request('collaboration', collaboration, headers, POST)
+
+    collaboration_id = result['id']
+
+    print(f'Created collaboration with id {collaboration_id}')
+
+    # Create user tied to organization
+    user = dict(ADMIN_USER)
+    user['organization_id'] = organization_id
+    result = request('user', user, headers, POST)
+
+    print(f'Created new user:\n{result}')
+
+    # Retrieve token for this new user
+    bearer_token = get_token('admin', 'admin')
+    headers = {'Authorization': bearer_token}
+
+    # Create nodes
+    for i in range(NUM_NODES):
+        result = request('node', {'collaboration_id': collaboration_id}, headers, POST)
+        node_id = result['id']
+
+        print(f'Created node with id: {node_id}')
 
 
 if __name__ == '__main__':
