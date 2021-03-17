3c4c8544109ca2ec771e7b21d75a6f398123e932
Robin Richardson <raarichardson@gmail.com>
2021-03-17 18:56:36 +0100

    Allow client-to-client communication

--- openvpn/server/setup_server.sh
+++ openvpn/server/setup_server.sh
@@ -2,7 +2,7 @@ OVPN_DATA="ovpn-data-vantage"
 CLIENTNAME="laptopclient"
 rm *.ovpn
 docker volume create --name $OVPN_DATA
-docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://VPN.SERVERNAME.COM
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://VPN.SERVERNAME.COM -c
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
 docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $CLIENTNAME nopass
