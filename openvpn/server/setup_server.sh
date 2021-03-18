361ad75609bd5ab8a749b2a067494c61e46a63a1
Robin Richardson <raarichardson@gmail.com>
2021-03-18 12:37:24 +0100

    No password for master cert either (for quick testing setup) and generate multiple client configs

--- openvpn/server/setup_server.sh
+++ openvpn/server/setup_server.sh
@@ -1,9 +1,16 @@
 OVPN_DATA="ovpn-data-vantage"
-CLIENTNAME="laptopclient"
-rm *.ovpn
+SERVER_IP= # Set this to the public IP or URL of your server
+
+rm -f *.ovpn
+
 docker volume create --name $OVPN_DATA
-docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://VPN.SERVERNAME.COM -c
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u $SERVER_IP -c
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki nopass
 docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $CLIENTNAME nopass
-docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $CLIENTNAME > ${CLIENTNAME}.ovpn
+
+# Create some client configs
+for CLIENTNAME in "client1" "client2" "client3" ;
+  do
+    docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $CLIENTNAME nopass
+    docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $CLIENTNAME > ${CLIENTNAME}.ovpn
+  done
